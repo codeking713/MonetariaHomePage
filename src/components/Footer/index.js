6144eb5c9ed3b9f2da8e832c430fc41a82b4e59f
Yash Tarkar <66407462+billafy@users.noreply.github.com>
2022-11-22 09:47:10 +0000

    mobile-version

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,14 +3,14 @@ import './styles.css';
 
 const Footer = () => {
     return (
-        <footer className='footer p-3 d-flex align-items-center'>
-            <div className='d-flex align-items-center justify-content-between gap-5 home-container'>
-                <div className='footer__links d-flex'>
-                    <a className='ms-auto text-white text-decoration-none px-4' href='/'>Ecosystem</a>
-                    <a className='ms-auto text-white text-decoration-none px-4' href='/'>Tokenomics</a>
-                    <a className='ms-auto text-white text-decoration-none px-4' href='/'>Governance</a>
-                    <a className='ms-auto text-white text-decoration-none ps-4' href='/'>Developers</a>
-                    <a className='ms-auto text-white text-decoration-none px-4' href='/'>About</a>
+        <footer className='footer d-flex align-items-center'>
+            <div className='footer__inner d-flex align-items-center justify-content-between gap-5 home-container'>
+                <div className='footer__links d-flex gap-5'>
+                    <a className='ms-auto text-white text-decoration-none' href='/'>Ecosystem</a>
+                    <a className='ms-auto text-white text-decoration-none' href='/'>Tokenomics</a>
+                    <a className='ms-auto text-white text-decoration-none' href='/'>Governance</a>
+                    <a className='ms-auto text-white text-decoration-none' href='/'>Developers</a>
+                    <a className='ms-auto text-white text-decoration-none' href='/'>About</a>
                 </div>
                 <div className='footer__social text-light d-flex align-items-center gap-4 fs-5'>
                     <FaGithub/>
